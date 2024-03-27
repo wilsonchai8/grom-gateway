8dd02c5725c723fdee1924929adb441083ff71be
chaiyuxiao <yuxiao.chai@yingxiong.com>
2024-03-27 14:32:27 +0800

    release:dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,6 @@
 FROM openresty/openresty:1.19.9.1-centos
 ADD nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
+ADD conf.d /etc/nginx/conf.d
+ADD dist /usr/local/openresty/nginx/html
+ADD lua /usr/local/openresty/nginx/lua
+ADD lib /usr/local/openresty/nginx/lib/resty
--- lua/access_entrance.lua
+++ lua/access_entrance.lua
@@ -14,29 +14,34 @@ local upstreams = {
     config = 'api-config',
 }
 
-local uri_table = split(ngx.var.uri)
-
-
-if table.getn(uri_table) < 3 or uri_table[1] ~= 'api' then
-    ngx.exit(400)
+local function main()
+    local uri_table = split(ngx.var.uri)
+    
+    
+    if table.getn(uri_table) < 3 or uri_table[1] ~= 'api' then
+        ngx.exit(400)
+    end
+    
+    if upstreams[uri_table[2]] == nil then
+        ngx.exit(416)
+    end
+    
+    local authorization = ngx.req.get_headers()['Authorization']
+    if (authorization == '' or authorization == nil) and 
+       (ngx.var.cookie_auth == '' or ngx.var.cookie_auth == nil) and 
+        uri_table[3] ~= 'auth' then
+        ngx.exit(401)
+    end
+    
+    
+    if uri_table[3] ~= 'auth' then
+        local res = check.auth_check(ngx.var.request_method)
+    end
+    
+    local location_format = string.gsub(ngx.var.request_uri, '/api/'..uri_table[2] , '', 1)
+    local real_uri = upstreams[uri_table[2]]..location_format
+    
+    ngx.var.my_upstream = real_uri
 end
 
-if upstreams[uri_table[2]] == nil then
-    ngx.exit(416)
-end
-
-if (ngx.var.cookie_token == '' or ngx.var.cookie_token == nil) and 
-   (ngx.var.cookie_auth == '' or ngx.var.cookie_auth == nil) and 
-    uri_table[3] ~= 'auth' then
-    ngx.exit(401)
-end
-
-
-if uri_table[3] ~= 'auth' then
-    local res = check.auth_check(ngx.var.request_method)
-end
-
-local location_format = string.gsub(ngx.var.request_uri, '/api/'..uri_table[2] , '', 1)
-local real_uri = upstreams[uri_table[2]]..location_format
-
-ngx.var.my_upstream = real_uri
+main()
